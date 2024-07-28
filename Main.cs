a4cf601f068abac1956363bf597c096270bb531e
moses <mosesberco1@gmail.com>
2024-07-28 21:34:11 +0300

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace final_project
+{
+    static class main
+    {
+        [STAThread]
+        public static void  Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var new_form = new EnglishGame2.GameForm();
+            //var hebrew_form = new Hebrew_Memory_Matching_Game.
+            Application.Run(new_form);
+        }
+    }
+}
