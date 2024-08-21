5e4fd133737e5bb8e39c3555ac87100da689631d
udivak <udivak9@gmail.com>
2024-08-21 18:46:41 +0300

    now finish for real

--- Main.cs
+++ Main.cs
@@ -9,29 +9,8 @@ namespace final_project
         public static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            var DB =new Database();
-            //var store_form = new StoreForm(206872871, DB);
-            //var hebrew_game = new Register();
-            //DB.RegisterUser("moses", "bro", "1234", "m@g.c", "Male");
-           // DB.SetBalance(1234, 1000);
-            User user = new User("moses", "bro", "1234" , "m@g.c", "Male", 10_000);
-            //var store = new StoreForm(user);
-            //var new_form = new Game_Udi(user);
-            var new_form = new Login();
-            //var new_form = new Hebrew_Etai(user);
-            //Application.Run(store);
-            //new_form.ShowDialog();
-            //var hebrew_form = new Hebrew_Memory_Matching_Game.
-            Application.Run(new_form);
-           
-            //DB.Dispose();
-            //var game = new EnsglishBuildWordsGameMenu();
-            //var form = new Register();
-            //Application.Run(new_form);
-            //var game_Diana = new GameScreen();
-            //Application.Run(new GameScreen(user));
-            //Application.Run(new Menu(user));
-
+            var appLogin = new Login();
+            Application.Run(appLogin);         
         }
     }
 }
